98b380daf9027062ddfa067775cec6038858f97f
leroimerlin1 <zakdjbr.shopify@gmail.com>
2026-02-06 21:01:57 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,9 +1,11 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = '8041091140:AAGdu3oR3Ag1L_mx_MHytlX4OjfB9wwJ5jo';
+const token = 'NOUVEAU_TOKEN_ICI';
 const bot = new TelegramBot(token, { polling: true });
 
 bot.onText(/\/start/, (msg) => {
+  console.log("Commande /start reçue");
+
   bot.sendMessage(msg.chat.id, '🦖 Bienvenue sur DINO 76', {
     reply_markup: {
       inline_keyboard: [[
@@ -16,4 +18,8 @@ bot.onText(/\/start/, (msg) => {
       ]]
     }
   });
+});
+
+bot.on('message', (msg) => {
+  console.log("Message reçu :", msg.text);
 });
