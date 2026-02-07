f788dfc047d697f00f15283ec4308ad19f58bd14
leroimerlin1 <zakdjbr.shopify@gmail.com>
2026-02-08 00:20:43 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,25 +9,25 @@ products_choco = {
     "frozen": {
         "name": "🥶 FROZEN SIFT",
         "desc": "Garlic Cookie 🍪, Jelly Donuts 🍩, Cake 🍰\nPromo -25%",
-        "video": "videos/caliplates.mp4",
+        "video": "caliplates.mp4",
         "prices": ["2,5G 50€", "5G 90€", "10G 180€", "20G 350€", "25G 400€"]
     },
     "gaz": {
         "name": "⚡️ Gaz fruit 90u",
         "desc": "Papaya Dolce 🥭, Mimi Cheese 🧀",
-        "video": "videos/gaz.mp4",
+        "video": "gaz.mp4",
         "prices": ["10G 130€", "25G 240€", "50G 450€"]
     },
     "calimountain": {
         "name": "🧑‍🌾 CALIMOUNTAIN 120u",
         "desc": "Candy Gaz 🍬, Glitter Bomb 💣, Apple Mintz 🍏",
-        "video": "videos/120u.mp4",
+        "video": "120u.mp4",
         "prices": ["5G 70€", "10G 140€", "20G 260€", "25G 310€"]
     },
     "farm": {
         "name": "🥶 FRESH FROZEN SIFT",
         "desc": "PERMANENT MAKER x GELATO 41 ⛽️🍦",
-        "video": "videos/farm.mp4",
+        "video": "farm.mp4",
         "prices": ["5G 70€", "10G 140€", "20G 250€", "25G 300€"]
     }
 }
@@ -35,7 +35,7 @@ products_choco = {
 cali = {
     "name": "🇺🇸 Cali weed",
     "desc": "Runtz 🌈, Tropicana Strawberry 🌴🍓",
-    "video": "videos/cali.mp4",
+    "video": "cali.mp4",
     "prices": ["3G 40€", "5G 60€", "10G 120€", "20G 230€", "25G 300€"]
 }
 
